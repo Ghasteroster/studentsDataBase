6502d12168e0046b4c098b5b733313867d89f3b1
IMKosh <120642481+Ghasteroster@users.noreply.github.com>
2025-09-17 14:49:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -55,102 +55,8 @@ void displayStudentsByMajor(const std::vector<Student>& database, const std::str
     }
 }
 
-// TESTS
-
-// Тест для пустой базы данных
-TEST(DisplayStudentsByMajorTest, HandlesEmptyDatabase) {
-    std::vector<Student> empty_database;
-    std::streambuf* old_cout = std::cout.rdbuf(output_buffer.rdbuf());
-    
-    displayStudentsByMajor(empty_database, "Информатика");
-    
-    std::cout.rdbuf(old_cout);
-    
-    std::string output = buffer.str();
-    
-    EXPECT_NE(output.find("не найдены"), std::string::npos);
-}
-
-// Проверяем, что выводятся студенты с заданной специальностью
-TEST(DisplayStudentsByMajorTest, FoundStudents) {
-    std::vector<Student> db = {
-        {"Иван", 20, "Математика", 4.5},
-        {"Анна", 22, "Физика", 4.2},
-        {"Петр", 21, "Математика", 3.9}
-    };
-
-    std::ostringstream buffer;
-    std::streambuf* old_cout = std::cout.rdbuf(buffer.rdbuf());
-
-    displayStudentsByMajor(db, "Математика");
-
-    std::cout.rdbuf(old_cout);
-
-    std::string output = buffer.str();
-    EXPECT_NE(output.find("Иван"), std::string::npos);
-    EXPECT_NE(output.find("Петр"), std::string::npos);
-    EXPECT_EQ(output.find("Анна"), std::string::npos);
-}
-
-// Тест для поиска по несуществующей специальности
-TEST(DisplayStudentsByMajorTest, HandlesNonExistingMajor) {
-    std::vector<Student> db = {
-        {"Иван", 20, "Математика", 4.5},
-        {"Анна", 22, "Физика", 4.2},
-        {"Петр", 21, "Математика", 3.9}
-    };
-    
-    displayStudentsByMajor(db, "Химия");
-    
-    std::cout.rdbuf(old_cout);
-    
-    std::string output = buffer.str();
-    
-    // Проверяем сообщение о том, что студенты не найдены
-    EXPECT_NE(output.find("не найдены"), std::string::npos);
-}
-
-// Тест для проверки точного соответствия специальности
-TEST(DisplayStudentsByMajorTest, HandlesNonExistingMajor) {
-    std::vector<Student> db = {
-        {"Иван", 20, "Математика", 4.5},
-        {"Анна", 22, "Физика", 4.2},
-        {"Петр", 21, "Математика", 3.9}
-    };
-    
-    displayStudentsByMajor(db, "математика");
-    
-    std::cout.rdbuf(old_cout);
-    
-    std::string output = buffer.str();
-    
-    // Проверяем сообщение о том, что студенты не найдены
-    EXPECT_NE(output.find("не найдены"), std::string::npos);
-}
-
-// Тест для проверки вывода всех полей студентов
-TEST(DisplayStudentsByMajorTest, OutputsCorrectStudentFields) {
-    std::vector<Student> db = {
-        {"Иван", 20, "Математика", 4.5},
-        {"Анна", 22, "Физика", 4.2},
-        {"Петр", 21, "Математика", 3.9}
-    };
-    
-    displayStudentsByMajor(students, "Математика");
-    
-    std::cout.rdbuf(old_cout);
-    
-    std::string output = output_buffer.str();
-    
-    // Проверяем, что вывод содержит все необходимые поля
-    EXPECT_NE(output.find("Имя:"), std::string::npos);
-    EXPECT_NE(output.find("Возраст:"), std::string::npos);
-    EXPECT_NE(output.find("Средний балл:"), std::string::npos);
-}
-
-
-void runInteractiveMode() {
-    std::vector<Student> database;
+int main(int argc, char **argv) {
+     std::vector<Student> database;
 
     int choice;
     do {
@@ -195,16 +101,4 @@ void runInteractiveMode() {
     } while (choice != 0);
 
     return 0;
-}
-
-int main(int argc, char **argv) {
-    // Если есть аргументы командной строки, запускает тесты
-    if (argc > 1) {
-        ::testing::InitGoogleTest(&argc, argv);
-        return RUN_ALL_TESTS();
-    } else {
-        // Иначе запускает интерактивное меню
-        runInteractiveMode();
-        return 0;
-    }
 }
